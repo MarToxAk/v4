5feaf777f69429912135ade63c76e4eb2c7a8768
MarToxAk <junior.ilha@hotmail.com.br>
2019-11-14 21:43:18 -0300

    New Teste

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -16,11 +16,28 @@ def post_facebook_message(fbid, recevied_message):
                                 "payload": {
                                     "template_type": "button",
                                     "text": "Try the postback button!",
-                                    "buttons": [
+                                    "persistent_menu": [
                                         {
-                                            "type": "postback",
-                                            "title": "Postback Button",
-                                            "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                                            "locale": "default",
+                                            "composer_input_disabled": false,
+                                            "call_to_actions": [
+                                                {
+                                                    "type": "postback",
+                                                    "title": "Talk to an agent",
+                                                    "payload": "CARE_HELP"
+                                                },
+                                                {
+                                                    "type": "postback",
+                                                    "title": "Outfit suggestions",
+                                                    "payload": "CURATION"
+                                                },
+                                                {
+                                                    "type": "web_url",
+                                                    "title": "Shop now",
+                                                    "url": "https://www.originalcoastclothing.com/",
+                                                    "webview_height_ratio": "full"
+                                                }
+                                            ]
                                         }
                                     ]
                                 }
