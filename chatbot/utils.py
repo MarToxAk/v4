c51b791453b10b2a48bcbef3c6526dd2e4079bf5
martox <junior.ilha@hotmail.com.br>
2019-11-15 09:02:58 -0300

    wqw

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -8,23 +8,39 @@ def post_facebook_message(fbid, recevied_message):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?' \
                        'access_token={}'.format(settings.ACCESS_TOKEN)
     teste = json.dumps({"recipient": {"id": fbid},
-                        "messaging_type": "RESPONSE",
-                        "message": {
-                            "text": "Pick a color:",
-                            "quick_replies": [
-                                {
-                                    "content_type": "text",
-                                    "title": "Red",
-                                    "payload": "<POSTBACK_PAYLOAD1>",
-                                    "image_url": "http://example.com/img/red.png"
-                                }, {
-                                    "content_type": "text",
-                                    "title": "Green",
-                                    "payload": "<POSTBACK_PAYLOAD2>",
-                                    "image_url": "http://example.com/img/green.png"
-                                }
-                            ]
-                        }
+                        "message":{
+                            "attachment":{
+                              "type":"template",
+                              "payload":{
+                                "template_type":"generic",
+                                "elements":[
+                                   {
+                                    "title":"Welcome!",
+                                    "image_url":"https://petersfancybrownhats.com/company_image.png",
+                                    "subtitle":"We have the right hat for everyone.",
+                                    "default_action": {
+                                      "type": "web_url",
+                                      "url": "https://petersfancybrownhats.com/view?item=103",
+                                      "messenger_extensions": false,
+                                      "webview_height_ratio": "tall",
+                                      "fallback_url": "https://petersfancybrownhats.com/"
+                                    },
+                                    "buttons":[
+                                      {
+                                        "type":"web_url",
+                                        "url":"https://petersfancybrownhats.com",
+                                        "title":"View Website"
+                                      },{
+                                        "type":"postback",
+                                        "title":"Start Chatting",
+                                        "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                                      }
+                                    ]
+                                  }
+                                ]
+                              }
+                            }
+                          }
                         }
                        )
     status = requests.post(post_message_url,
