a696b3edc70101b650ba615e3bcb996597ab8721
MarToxAk <junior.ilha@hotmail.com.br>
2019-11-14 21:20:54 -0300

    New Teste

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -9,7 +9,9 @@ def post_facebook_message(fbid, recevied_message):
                        'access_token={}'.format(settings.ACCESS_TOKEN)
     response_msg = json.dumps({"recipient": {"id": fbid},
                                "message": {"text": f'1{recevied_message}'}})
+    teste = json.dumps({"recipient": {"id": fbid},
+                        {"message": {"attachment": {"type": "template","payload": {"template_type": "button","text": "What do you want to do next?","buttons": [{"type": "web_url","url": "https://www.messenger.com","title": "Visit Messenger"}}}}}}})
     status = requests.post(post_message_url,
                            headers={"Content-Type": "application/json"},
-                           data=response_msg)
+                           data=teste)
     pprint(status.json())
