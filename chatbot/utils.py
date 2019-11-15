fb5be0ba819598a871d84b73c80ebf462c351179
martox <junior.ilha@hotmail.com.br>
2019-11-15 08:37:23 -0300

    dsa

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -9,71 +9,25 @@ def post_facebook_message(fbid, recevied_message):
                        'access_token={}'.format(settings.ACCESS_TOKEN)
     teste = json.dumps({"recipient": {"id": fbid},
                         "message": {
-                            "attachment": {
-                              "type": "template",
-                              "payload": {
-                                "template_type": "airline_boardingpass",
-                                "intro_message": "You are checked in.",
-                                "locale": "en_US",
-                                "boarding_pass": [
-                                  {
-                                    "passenger_name": "SMITH\/NICOLAS",
-                                    "pnr_number": "CG4X7U",
-                                    "seat": "74J",
-                                    "logo_image_url": "https://s2.logaster.com/static/v3/img/products/logo.png",
-                                    "header_image_url": "https://s2.logaster.com/static/v3/img/products/logo.png",
-                                    "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
-                                    "above_bar_code_image_url": "https://s2.logaster.com/static/v3/img/products/logo.png",
-                                    "auxiliary_fields": [
-                                      {
-                                        "label": "Terminal",
-                                        "value": "T1"
-                                      },
-                                      {
-                                        "label": "Departure",
-                                        "value": "30OCT 19:05"
-                                      }
-                                    ],
-                                    "secondary_fields": [
-                                      {
-                                        "label": "Boarding",
-                                        "value": "18:30"
-                                      },
-                                      {
-                                        "label": "Gate",
-                                        "value": "D57"
-                                      },
-                                      {
-                                        "label": "Seat",
-                                        "value": "74J"
-                                      },
-                                      {
-                                        "label": "Sec.Nr.",
-                                        "value": "003"
-                                      }
-                                    ],
-                                    "flight_info": {
-                                      "flight_number": "KL0642",
-                                      "departure_airport": {
-                                        "airport_code": "JFK",
-                                        "city": "New York",
-                                        "terminal": "T1",
-                                        "gate": "D57"
-                                      },
-                                      "arrival_airport": {
-                                        "airport_code": "AMS",
-                                        "city": "Amsterdam"
-                                      },
-                                      "flight_schedule": {
-                                        "departure_time": "2016-01-02T19:05",
-                                        "arrival_time": "2016-01-05T17:30"
-                                      }
+                          "attachment": {
+                            "type": "template",
+                            "payload": {
+                              "template_type": "open_graph",
+                              "elements": [
+                                {
+                                  "url": "https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb",
+                                  "buttons": [
+                                    {
+                                      "type": "web_url",
+                                      "url": "https://en.wikipedia.org/wiki/Rickrolling",
+                                      "title": "View More"
                                     }
-                                  }
-                                ]
-                              }
+                                  ]
+                                }
+                              ]
                             }
                           }
+                        }
                         }
                        )
     status = requests.post(post_message_url,
