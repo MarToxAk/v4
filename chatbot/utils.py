7e55131e3ed936b0c8596e1a8f4eeb1ac933d58d
martox <junior.ilha@hotmail.com.br>
2019-11-15 07:41:04 -0300

    dsa

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from pprint import pprint
 
 
-def post_facebook_message(fbid):
+def post_facebook_message(fbid, recevied_message):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?' \
                        'access_token={}'.format(settings.ACCESS_TOKEN)
     teste = json.dumps({"recipient": {"id": fbid},
