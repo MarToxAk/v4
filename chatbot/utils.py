65d12a46df724ac30887d06d19edd8b77879197e
Emma Paris <eparis@atlassian.com>
2019-11-18 02:48:57 -0300

    dasda

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -8,8 +8,22 @@ def post_facebook_message(fbid, recevied_message):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?' \
                        'access_token={}'.format(settings.ACCESS_TOKEN)
     teste = json.dumps({"recipient": {"id": fbid},
+                        "messaging_type": "RESPONSE",
                         "message": {
-                            "text": "Teste de Mensagem"
+                            "text": "Bem-Vindo a ILHA BABY. Responda com uma das opções abaixo.",
+                            "quick_replies":[
+                            {
+                                "content_type": "text",
+                                "title": "Quero Comprar",
+                                "payload": "<POSTBACK_PAYLOAD>",
+                                "image_url": "https://cdn.icon-icons.com/icons2/67/PNG/512/shoppingcart_compra_13339.png"
+                            }, {
+                                "content_type": "text",
+                                "title": "Green",
+                                "payload": "<POSTBACK_PAYLOAD>",
+                                "image_url": "http://example.com/img/green.png"
+                            }
+                        ]
                         }
                         }
                        )
