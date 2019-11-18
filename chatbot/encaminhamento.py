d94ea22cfa26433a8e08e2d203cd2cc9cd7f88fc
Emma Paris <eparis@atlassian.com>
2019-11-18 05:47:14 -0300

    dasda

--- chatbot/encaminhamento.py
+++ chatbot/encaminhamento.py
@@ -1,12 +1,13 @@
 from pprint import pprint
 from .utils import post_facebook_message
-
+list_oi = ['Ola', 'oi', 'Oi', 'OI', 'oI']
 def test(incoming_message):
     for entry in incoming_message['entry']:
         for message in entry['messaging']:
             pprint(message)
+
             pprint(message['message']['text'])
-            if 'oi' in message['message']['text']:
+            if list_oi in message['message']['text']:
                 post_facebook_message(message['sender']['id'])
             else:
                 pass
