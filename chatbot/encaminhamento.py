3d4f6b47b80c2e24ad9ff54533a4f9fdcce58b45
martox <junior.ilha@hotmail.com.br>
2019-11-19 08:46:24 -0300

    wqw

--- chatbot/encaminhamento.py
+++ chatbot/encaminhamento.py
@@ -1,8 +1,11 @@
 from pprint import pprint
-from .utils import post_facebook_message, post_facebook_message_sair
+from .utils import post_facebook_message, post_facebook_message_sair, post_facebook_message_date, post_facebook_message_checkin
 import urllib.request
 import json
 from django.conf import settings
+import re
+
+
 
 list_oi = ['Ola', 'oi', 'Oi', 'OI', 'oI']
 
@@ -28,5 +31,17 @@ def test(incoming_message):
                     first_name = json.loads(url.read().decode('utf-8'))
                     pprint(first_name)
                 post_facebook_message_sair(message['sender']['id'], first_name['first_name'])
+            elif re.match(r'\d\d/\d\d/\d\d', f"{message['message']['text']}") or re.match(r'\d\d/\d\d/\d\d\d\d', f"{message['message']['text']}"):
+                with urllib.request.urlopen(
+                        f"https://graph.facebook.com/{message['sender']['id']}?fields=first_name,last_name,profile_pic&access_token={settings.ACCESS_TOKEN}") as url:
+                    first_name = json.loads(url.read().decode('utf-8'))
+                    pprint(first_name)
+                post_facebook_message_checkin(message['sender']['id'], first_name['first_name'])
+            elif 'Data Check-in' in message['message']['text']:
+                with urllib.request.urlopen(
+                        f"https://graph.facebook.com/{message['sender']['id']}?fields=first_name,last_name,profile_pic&access_token={settings.ACCESS_TOKEN}") as url:
+                    first_name = json.loads(url.read().decode('utf-8'))
+                    pprint(first_name)
+                post_facebook_message_date(message['sender']['id'], first_name['first_name'])
             else:
                 pass
--- chatbot/utils.py
+++ chatbot/utils.py
@@ -2,6 +2,7 @@ import json
 import requests
 from django.conf import settings
 from pprint import pprint
+import datetime
 
 
 def post_facebook_message(fbid, first_name):
@@ -17,6 +18,11 @@ def post_facebook_message(fbid, first_name):
                                 "title": "Quero Comprar",
                                 "payload": "comprar",
                                 "image_url": "https://cdn.icon-icons.com/icons2/67/PNG/512/shoppingcart_compra_13339.png"
+                            }, {
+                                "content_type": "text",
+                                "title": "Data Check-in",
+                                "payload": "Check-in",
+                                "image_url": "https://www.netclipart.com/pp/m/104-1044961_calendar-icon-png-date-events-icon-white-png.png"
                             }, {
                                 "content_type": "text",
                                 "title": "Sair",
@@ -37,6 +43,34 @@ def post_facebook_message_sair(fbid, first_name):
     teste = json.dumps({"recipient": {"id": fbid},
                         "message": {
                             "text": f"*BOT ILHA BABY*: Obrigado pela Atenção {first_name}.",
+                        }
+                        }
+                       )
+    status = requests.post(post_message_url,
+                           headers={"Content-Type": "application/json"},
+                           data=teste)
+    pprint(status.json())
+
+def post_facebook_message_checkin(fbid, first_name):
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?' \
+                       'access_token={}'.format(settings.ACCESS_TOKEN)
+    teste = json.dumps({"recipient": {"id": fbid},
+                        "message": {
+                            "text": f"*BOT ILHA BABY*: Agora {first_name}, Digite a data que você quer entrar no seguinte formato DD/MM/AAAA.(Exemplo:. {datetime.date.strftime(datetime.date.today(), '%d/%m/%Y')}.",
+                        }
+                        }
+                       )
+    status = requests.post(post_message_url,
+                           headers={"Content-Type": "application/json"},
+                           data=teste)
+    pprint(status.json())
+
+def post_facebook_message_date(fbid, first_name):
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?' \
+                       'access_token={}'.format(settings.ACCESS_TOKEN)
+    teste = json.dumps({"recipient": {"id": fbid},
+                        "message": {
+                            "text": f"*BOT ILHA BABY*: Obrigado pela Atenção {first_name}, Ok tudo Certo.",
                         }
                         }
                        )
